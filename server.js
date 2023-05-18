3730390118c0ba6f7cf02c46b470b73b05fbec81
Ivan-Tkachuk <tkachuchella@gmail.com>
2023-05-18 13:17:55 +0300

    init project

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+const app = require('./app');
+
+const DB_HOST = "mongodb+srv://ivan:6bBfwK3bs6TL4*.@cluster0.0qmv3dz.mongodb.net/contacts_reader?retryWrites=true&w=majority";
+
+// mongoose.connect('strictQuery', true);
+
+mongoose.connect(DB_HOST).then(() => {
+  app.listen(3000, () => {
+  console.log("Database connection successful");
 })
+}).catch(error => {
+  console.log(error.message);
+  process.exit(1);
+});
+
+
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000")
+// })
