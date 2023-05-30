cdaf2a654336c5803e2fa9512c08ae77058a8b62
Ivan-Tkachuk <tkachuchella@gmail.com>
2023-05-30 18:54:26 +0300

    almost doen hw-05

---
+++ middlewares/upload.js
@@ -0,0 +1,17 @@
+const multer = require("multer");
+const path = require("path");
+
+const tempDir = path.join(__dirname, "../", "temp");
+
+const mupterConfig = multer.diskStorage({
+    destination: tempDir,
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({
+    storage: mupterConfig
+})
+
+module.exports = upload;
