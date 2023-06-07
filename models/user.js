6516fb5268f10aae3e8e5a6241f6c68c8c71fec3
Ivan-Tkachuk <tkachuchella@gmail.com>
2023-06-07 19:39:23 +0300

    doen hw06 without additional task (docker)

--- models/user.js
+++ models/user.js
@@ -31,6 +31,14 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+      required: [true, 'Verify token is required'],
+    },
   },
   { versionKey: false, timestamps: true }
 );
@@ -51,6 +59,12 @@ const loginSchema = Joi.object({
   password: Joi.string().min(6).required(),
 });
 
+
+const emailSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+});
+
+
 const updateSubscriptionSchema = Joi.object({
   subscription: Joi.string().valid("starter", "pro", "business").required(),
 });
@@ -59,6 +73,7 @@ const schemas = {
   registerSchema,
   loginSchema,
   updateSubscriptionSchema,
+  emailSchema,
 };
 
 const User = model("user", userSchema);
